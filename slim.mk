f0850f84ca346b23519432b086d8213d5bc74e50
Josue Rivera <prbassplayer@gmail.com>
2013-08-15 22:13:23 +0200

    T0ltetmo: Cleanup on slim.mk
    Change-Id: I6bde4cb4c91075ab26092fbc043802149735fc6c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -17,9 +17,6 @@ $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-# Inherit device settings
-$(call inherit-product, vendor/slim/config/common_sgs.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0ltetmo
 PRODUCT_NAME := slim_t0ltetmo
