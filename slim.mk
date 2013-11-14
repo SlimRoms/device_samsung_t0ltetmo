f41009d5c817c327bd3cfd7c4a6dad5cf8b2431e
Josue Rivera <prbassplayer@gmail.com>
2013-11-14 01:38:58 +0100

    T0ltetmo: Update dependencies and clean up mk
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -1,21 +1,21 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := t0ltetmo
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := t0ltetmo
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0ltetmo
