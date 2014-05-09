a4eb822cab82cbf3e64775dc55af8fbf34edb68c
Josue Rivera <prbassplayer@gmail.com>
2014-05-09 14:43:46 +0200

    T0ltetmo: Add missing configuration call
    Change-Id: Ief5e2534313825e2af1c55d38ae5982cc620a591
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -11,6 +11,9 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := t0ltetmo
 
